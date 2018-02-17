2667f726e61728a4cb8ac3281bfee9cf3bc1e359
seancalida <seancalida@gmail.com>
2018-02-17 18:29:45 -0500

    merged cognitive functions

--- js/main.js
+++ js/main.js
@@ -187,10 +187,10 @@ function onIceCandidate(pc, event) {
   if (event.candidate) {
     getOtherPc(pc).addIceCandidate(
       new RTCIceCandidate(event.candidate)
-    ).then(
-      function() {
-        onAddIceCandidateSuccess(pc);
-      },
+      ).then(
+        function() {
+          onAddIceCandidateSuccess(pc);
+        },
       function(err) {
         onAddIceCandidateError(pc, err);
       }
@@ -236,3 +236,62 @@ function trace(text) {
     console.log(text);
   }
 }
+
+function processImage() {
+  // **********************************************
+  // *** Update or verify the following values. ***
+  // **********************************************
+
+  // Replace the subscriptionKey string value with your valid subscription key.
+  var subscriptionKey = "8d3c23ec9e66474eb8c482a81d11dae6";
+
+  // Replace or verify the region.
+  //
+  // You must use the same region in your REST API call as you used to obtain your subscription keys.
+  // For example, if you obtained your subscription keys from the westus region, replace
+  // "westcentralus" in the URI below with "westus".
+  //
+  // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+  // a free trial subscription key, you should not need to change this region.
+  var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+  // Request parameters.
+  var params = {
+      "returnFaceAttributes": "smile",
+  };
+
+  // Display the image.
+  var sourceImageUrl = document.getElementById("inputImage").value;
+  document.querySelector("#sourceImage").src = sourceImageUrl;
+
+  // Perform the REST API call.
+  $.ajax({
+    url: uriBase + "?" + $.param(params),
+
+    // Request headers.
+    beforeSend: function(xhrObj){
+      xhrObj.setRequestHeader("Content-Type","application/json");
+      xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+    },
+
+    type: "POST",
+
+    // Request body.
+    data: '{"url": ' + '"' + sourceImageUrl + '"}',
+  })
+
+  .done(function(data) {
+  // Show formatted JSON on webpage.
+    $("#smileval").text(data[0].faceAttributes.smile);
+    $("#responseTextArea").val(JSON.stringify(data, null, 1));
+
+  })
+
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    // Display error message.
+    var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+    errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+    jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+    alert(errorString);
+  });
+};
